b2b11546d5b5987cba98853f6f484cc5f271f1d6
sourabh2104 <sourabh2104@ubuntu>
2024-07-22 12:26:50 +0530

    feat: Initialize App

---
+++ README.md
@@ -0,0 +1,7 @@
+## Custom1
+
+Report Scripting
+
+#### License
+
+MIT
---
+++ custom1/config/desktop.py
@@ -0,0 +1,10 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "Custom1",
+			"type": "module",
+			"label": _("Custom1")
+		}
+	]
